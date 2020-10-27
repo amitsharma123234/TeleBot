99909a1c3120aaa6b8b1a5c54ab92c20d3d279f4
Lovedeep ViRk <73482922+Lovedeep-ViRk@users.noreply.github.com>
2020-10-27 12:54:48 +0530

    Update telebotConfig.py

--- userbot/telebotConfig.py
+++ userbot/telebotConfig.py
@@ -1,5 +1,5 @@
-#    TeleBot - UserBot
-#    Copyright (C) 2020 TeleBot
+#    DeepTeleBot - UserBot
+#    Copyright (C) 2020 DeepTeleBot
 
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as published by
